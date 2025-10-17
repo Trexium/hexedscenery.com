509d0502e28fd522a7dd3812a67ca07afb9ee56c
trexium <emil@gilljam.org>
2025-10-17 16:34:53 +0200

    Added besta test application form.

---
+++ HexedSceneryWebsite/SMTPInfo.cs
@@ -0,0 +1,10 @@
+﻿namespace HexedSceneryWebsite
+{
+    public class SMTPInfo
+    {
+        public string Host { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Recipient { get; set; }
+    }
+}
